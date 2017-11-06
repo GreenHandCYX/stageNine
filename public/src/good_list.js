3c6b962c4fab9929b9c44996a2be4b610b56d780
cyx <1094536944@qq.com>
2017-11-06 15:15:21 +0800

    完成商品分页展示

---
+++ public/src/good_list.js
@@ -0,0 +1,41 @@
+define(['jquery','template','./utils'],function($,template){
+
+
+
+    //用正则处理浏览器地址来获取当前页码
+    
+    //获取请求地址
+    var search = location.search || '';
+    //设置正则
+    var reg = /\?[a-z]+=(\d+)/;
+    //通过正则的exec方法获取页码
+    var page =reg.exec(search) && reg.exec(search)[1] || 1; 
+
+    //每页显示的数据条数
+    var size = 2;
+
+
+    $.ajax({
+        url:'/api/product/queryProductDetailList',
+        typr:'get',
+        data:{page:page,pageSize:size},
+        success:function(info){
+            //渲染商品列表
+            var html = template('tpl',info);
+            $('.goods').html(html);
+
+            //数据总条数
+            var total = info.total;
+            
+            //总页码
+            var pageLen = Math.ceil(total/size);
+
+            //渲染分页列表
+            var htmlPage = template('page',{
+                pageLen:pageLen,
+                page:page
+            })
+            $('.pagination').html(htmlPage)
+        }
+    })
+})
